c662a81fcf7c4811d6a55ef21248ca98036019f4
asmarais <rais.asma99@gmail.com>
2024-02-06 22:33:06 +0100

    i18n

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+
+
+class Config:
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+@app.route('/')
+def index():
+    return render_template('1-index.html')
+
+
+babel = Babel(app)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
